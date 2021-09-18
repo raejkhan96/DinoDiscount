7c1d9df3b8155ab317f8d33974bca9fdc75d3c77
SJ <sj.jega2@gmail.com>
2021-09-18 19:05:51 +0000

    Add user_favorite table

--- db/seeds/01_users.sql
+++ db/seeds/01_users.sql
@@ -1,6 +1,7 @@
 -- Users table seeds here (Example)
 INSERT INTO users (name, dob, email, post_code, phone_number, user_pic, password)
-VALUES ('Ian Malcolm', '1955-06-29', 'imalcolm@gmail.com', '123ABC', '123-456-7891', 'https://static.wikia.nocookie.net/jurassicpark/images/c/c6/JP-IanMalcolm.jpg/revision/latest?cb=20111124071517', 'password'),
+VALUES
+('Ian Malcolm', '1955-06-29', 'imalcolm@gmail.com', '123ABC', '123-456-7891', 'https://static.wikia.nocookie.net/jurassicpark/images/c/c6/JP-IanMalcolm.jpg/revision/latest?cb=20111124071517', 'password'),
 ('John Hammond', '1928-03-14', 'jhammond@gmail.com', '124ABC', '123-456-7892', 'https://static.wikia.nocookie.net/jurassicpark/images/f/f8/JP-JohnHammond.jpg/revision/latest?cb=20080901014643', 'password'),
 ('Chris Pratt', '1979-06-21', 'cpratt@gmail.com', '125ABC', '123-456-7893', 'https://encrypted-tbn2.gstatic.com/images?q=tbn:ANd9GcRzGnI__Ul05wKYDq3QYeoy1QbXXdWMvJpLezay515H6OiORtH-', 'password');
 
--- db/seeds/02_listings.sql
+++ db/seeds/02_listings.sql
@@ -1,6 +1,7 @@
 -- Widgets table seeds here (Example)
 INSERT INTO listings (name, price, description, picture, type, time_period, user_id)
-VALUES ('T-rex', 1000000000, 'Scary dino', 'shorturl.at/bvDI1', 1, 3, 1),
+VALUES
+('T-rex', 1000000000, 'Scary dino', 'shorturl.at/bvDI1', 1, 3, 1),
 ('Triceratops', 1000000000, 'Three horned dino', 'shorturl.at/goCX0', 2,3, 2),
 ('Stegosaurus', 1000000000, 'A large, slow moving plant-eater', 'shorturl.at/swCIY', 2, 2, 3),
 ('Coloradisaurus', 100, 'Coloradisaurus', 'shorturl.at/gluS4', 3, 1, 3);
