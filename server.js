e0906cf223310d5183e6662a54e7a6ecc91a8d4b
SJ <sj.jega2@gmail.com>
2021-09-19 03:46:14 +0000

    Add logic to search function

--- server.js
+++ server.js
@@ -35,11 +35,13 @@ app.use(express.static("public"));
 // Note: Feel free to replace the example routes below with your own
 const usersRoutes = require("./routes/users");
 const widgetsRoutes = require("./routes/widgets");
+const listingsRoutes = require("./routes/listings");
 
 // Mount all resource routes
 // Note: Feel free to replace the example routes below with your own
 app.use("/api/users", usersRoutes(db));
 app.use("/api/widgets", widgetsRoutes(db));
+app.use("/api/listings", listingsRoutes(db));
 // Note: mount other resources here, using the same pattern above
 
 
@@ -51,5 +53,5 @@ app.get("/", (req, res) => {
 });
 
 app.listen(PORT, () => {
-  console.log(`Example app listening on port ${PORT}`);
+  console.log(`Server listening on port ${PORT}`);
 });
